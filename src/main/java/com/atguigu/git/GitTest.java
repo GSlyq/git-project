22d024a00196e00e79b26f934dd78efe8ab6aef6
lyq <lyq@163.com>
2023-10-25 16:31:49 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ package main.java.com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
