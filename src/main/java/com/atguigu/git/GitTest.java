33851c1adee4cba5bd12fc9720e3ce450899a44a
lyq <lyq@163.com>
2023-10-25 16:29:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package main.java.com.atguigu.git;
+
+/**
+ * ClassName:GitTest
+ * Package:main.java.com.atguigu.git
+ * Description:
+ *
+ * @Author lyq
+ * @Create 2023/10/25-16:24
+ * Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
